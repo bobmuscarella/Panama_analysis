f97ce4c7e44100498fe9a62e87671cea404b5358
Bob Muscarella <bob.muscarella@gmail.com>
2015-12-11 12:00:33 +0100

    update

--- Growth_multiplot_12.11.R
+++ Growth_multiplot_12.11.R
@@ -7,7 +7,7 @@ library(jagsUI)
 z.score <- function (data) {
   xm<- mean (data, na.rm=TRUE)
   xsd<-sd(data, na.rm=TRUE)
-  xtrans<-(data-xm)/(2*xsd)	
+  xtrans<-(data-xm)/(2*xsd)  
   return(xtrans)
 }
 
@@ -49,96 +49,45 @@ trait <- traits[i]
 d <- d[!is.na (d[,trait]),]  					
 d <- droplevels(d)
 
-################################################################
-####    Standardize and Center coefficients (within plots?)    ####
-################################################################
-d <- d[order(d$plot, d$spcode, d$id, d$census),]
 
-d$growth.z <- unlist(tapply(d$growth, d$plot, scale, center=F))
-d$log.nci.z <- z.score(d$log.nci)
-#d$log.nci.z <- unlist(tapply(d$log.nci, d$plot, z.score))
-d$log.dbh.z <- z.score(d$log.dbh)
-
-d$log.tnci.wsg.z <- z.score(d$log.tnci.wsg)
-d$log.tnci.log.ldmc.z <- z.score(d$log.tnci.log.ldmc)
-d$log.tnci.log.lma.z <- z.score(d$log.tnci.log.lma)
-d$log.tnci.log.seed.z <- z.score(d$log.tnci.log.seed)
-d$log.tnci.hmax.z <- z.score(d$log.tnci.hmax)
-#d$log.tnci.wsg.z <- unlist(tapply(d$log.tnci.wsg, d$plot, z.score))
-#d$log.tnci.log.ldmc.z <- unlist(tapply(d$log.tnci.log.ldmc, d$plot, z.score))
-#d$log.tnci.log.lma.z <- unlist(tapply(d$log.tnci.log.lma, d$plot, z.score))
-#d$log.tnci.log.seed.z <- unlist(tapply(d$log.tnci.log.seed, d$plot, z.score))
-#d$log.tnci.hmax.z <- unlist(tapply(d$log.tnci.hmax, d$plot, z.score))
-
-d$log.unci.wsg.z <- z.score(d$log.unci.wsg)
-d$log.unci.log.ldmc.z <- z.score(d$log.unci.log.ldmc)
-d$log.unci.log.lma.z <- z.score(d$log.unci.log.lma)
-d$log.unci.log.seed.z <- z.score(d$log.unci.log.seed)
-d$log.unci.hmax.z <- z.score(d$log.unci.hmax)
-#d$log.unci.wsg.z <- unlist(tapply(d$log.unci.wsg, d$plot, z.score))
-#d$log.unci.log.ldmc.z <- unlist(tapply(d$log.unci.log.ldmc, d$plot, z.score))
-#d$log.unci.log.lma.z <- unlist(tapply(d$log.unci.log.lma, d$plot, z.score))
-#d$log.unci.log.seed.z <- unlist(tapply(d$log.unci.log.seed, d$plot, z.score))
-#d$log.unci.hmax.z <- unlist(tapply(d$log.unci.hmax, d$plot, z.score))
-
-
-#################################################################################
-#### Standardize and Center coefficients (within plots, within size classes) ####
-#################################################################################
+######################################################################################
+#### Standardize and Center coefficients (within plots, within size classes 10cm) ####
+######################################################################################
 d$growth.z <- unlist(tapply(d$growth, d$plot, scale, center=F))
 
 ### Center / scale DBH within species, within size class
-size.classes <- tapply(d$dbh, paste(d$spcode, d$plot), quantile, 0.5)
-d$size.class <- ifelse(d$dbh <= size.classes[match(paste(d$spcode, d$plot), names(size.classes))], 1, 2)
-d <- d[order(d$plot, d$spcode, d$size.class, d$id, d$census),]
-
-sp.size.class <- paste(d$spcode, d$size.class, sep=".")
-for (i in 1:length(unique(sp.size.class))){
-  tmp <- sort(unique(sp.size.class))[i]
-  d$log.dbh.z[sp.size.class %in% tmp] <- z.score(d$log.dbh[sp.size.class %in% tmp])
-}
-### SET DIAM.z OF SINGLETON SPECIES (size classes) TO ZERO
-d$log.dbh.z[is.na(d$log.dbh.z)] <- 0
+d$sc <- ifelse(d$dbh <= 100, 1, 2)
+d <- d[order(d$plot, d$spcode, d$sc, d$id, d$census),]
 
 ### Center / scale other variableswithin size class
 for (i in 1:2){
-  d$log.nci.z[d$size.class %in% i] <- z.score(d$log.nci[d$size.class %in% i])
-  d$log.tnci.wsg.z[d$size.class %in% i] <- z.score(d$log.tnci.wsg[d$size.class %in% i])
-  d$log.tnci.log.ldmc.z[d$size.class %in% i] <- z.score(d$log.tnci.log.ldmc[d$size.class %in% i])
-  d$log.tnci.log.lma.z[d$size.class %in% i] <- z.score(d$log.tnci.log.lma[d$size.class %in% i])
-  d$log.tnci.log.seed.z[d$size.class %in% i] <- z.score(d$log.tnci.log.seed[d$size.class %in% i])
-  d$log.tnci.hmax.z[d$size.class %in% i] <- z.score(d$log.tnci.hmax[d$size.class %in% i])
+  d$log.dbh.z[d$sc %in% i] <- unlist(tapply(d$log.dbh[d$sc %in% i], d$plot[d$sc %in% i], z.score))
+  d$log.nci.z[d$sc %in% i] <- unlist(tapply(d$log.nci[d$sc %in% i], d$plot[d$sc %in% i], z.score))
+  d$log.tnci.wsg.z[d$sc %in% i] <- unlist(tapply(d$log.tnci.wsg[d$sc %in% i], d$plot[d$sc %in% i], z.score))
+  d$log.tnci.log.ldmc.z[d$sc %in% i] <- unlist(tapply(d$log.tnci.log.ldmc[d$sc %in% i], d$plot[d$sc %in% i], z.score))
+  d$log.tnci.log.lma.z[d$sc %in% i] <- unlist(tapply(d$log.tnci.log.lma[d$sc %in% i], d$plot[d$sc %in% i], z.score))
+  d$log.tnci.log.seed.z[d$sc %in% i] <- unlist(tapply(d$log.tnci.log.seed[d$sc %in% i], d$plot[d$sc %in% i], z.score))
+  d$log.tnci.hmax.z[d$sc %in% i] <- unlist(tapply(d$log.tnci.hmax[d$sc %in% i], d$plot[d$sc %in% i], z.score))
 }
 
 
 #################################
 ####    PREP Continues...    ####
 #################################
-
 d$plot <- substring(d$plot, 1, 3)
 
 # CHOOSE A SIZE CLASS TO WORK WITH...
 # for(sc in 1:2) {
 sc <- 1
 
-d <- d[d$size.class %in% sc,]
+d <- d[d$sc %in% sc,]
 
-
-# CHOOSE A PLOT TO WORK WITH ONE PLOT AT A TIME...
-# for(p in 1:3) {
-plots <- c('bci','coc','she')
-p <- 2
-focal.plot <- plots[p]
-
-d <- d[,c('spcode','plot','size.class','census','growth.z','log.dbh.z','id','log.nci.z', 
+d <- d[,c('spcode','plot','sc','census','growth.z','log.dbh.z','id','log.nci.z', 
           trait, 
           paste('log.tnci', trait, 'z', sep='.'))]
 
-d <- d[d$plot %in% focal.plot,]
-
-
 # SAMPLE DATA FOR EXPLORATORY...
-# d <- d[sample(1:nrow(d), 1000),]
+# d <- d[sample(1:nrow(d), 5000),]
 # d <- droplevels(d)
 # rownames(d) <- NULL
 
@@ -162,13 +111,15 @@ data = list (
   ntree = nrow(d),
   nindiv = length(unique(d$id)),
   nspecies = length(levels(d$speciesxplot)),
+  nplot = length(levels(as.factor(d$plot))),
   growth = as.numeric(d$growth.z),
   nci = as.numeric(d[,'log.nci.z']),
-  tnci = as.numeric(d[,paste('log.tnci.', trait, '.z', sep='')]),
+#  tnci = as.numeric(d[,paste('log.tnci.', trait, '.z', sep='')]),
   dbh = as.numeric(d$log.dbh.z),
   trait = z.score(tapply(d[,trait], d$speciesxplot, mean)),
   indiv = d$indiv,
-  species = as.numeric(d$speciesxplot)
+  species = as.numeric(d$speciesxplot),
+  plot = as.numeric(as.factor(substring(names(tapply(d[,trait], d$speciesxplot, mean)),1,3)))
 )
 
 
@@ -181,7 +132,7 @@ if(pc==T){
   setwd("/Users/Bob/Projects/Postdoc/Panama/MODELS")
 }
 
-sink("growth_2level_NCI_NCIXDBH.bug")
+sink("growth_3level_NCI.bug")
 
 cat(" model {
     
@@ -191,86 +142,45 @@ cat(" model {
     
     mu[i] <- beta.1[species[i]]
              + beta.2[species[i]] * nci[i]
-             + beta.3[species[i]] * (dbh[i]
-             + beta.4[species[i]] * nci[i] * dbh[i]
+             + beta.3[species[i]] * dbh[i]
              + indiv.effect[indiv[i]]
     }
     
     for( j in 1:nspecies ) {
-      beta.1[j] ~ dnorm(mu.beta[1] + beta.t[1] * trait[j], tau[2])
-      beta.2[j] ~ dnorm(mu.beta[2] + beta.t[2] * trait[j], tau[3])
-      beta.3[j] ~ dnorm(mu.beta[3], tau[4])
-      beta.4[j] ~ dnorm(mu.beta[4] + beta.t[3] * trait[j], tau[5])
+      beta.1[j] ~ dnorm(mu.beta.1[plot[j]] + beta.t.1[plot[j]] * trait[j], tau[2])
+      beta.2[j] ~ dnorm(mu.beta.2[plot[j]] + beta.t.2[plot[j]] * trait[j], tau[3])
+      beta.3[j] ~ dnorm(mu.beta.3[plot[j]], tau[4])
     }
     
+    for( p in 1:nplot ) {
+      mu.beta.1[p] ~ dnorm(mu.beta[1], tau[5])
+      mu.beta.2[p] ~ dnorm(mu.beta[2], tau[6])
+      mu.beta.3[p] ~ dnorm(mu.beta[3], tau[7])
+      beta.t.1[p] ~ dnorm(beta.t[1], tau[8])
+      beta.t.2[p] ~ dnorm(beta.t[2], tau[9])
+    }
+
     for( i.a in 1:nindiv ) {
-      indiv.effect[i.a] ~ dnorm(0, tau[6])
+    indiv.effect[i.a] ~ dnorm(0, tau[10])
     }
     
-    for( b in 1:3 ) {
-      beta.t[b] ~ dnorm(0, 1E-4)
+    for( b in 1:2 ) {
+    beta.t[b] ~ dnorm(0, 1E-4)
     }
     
-    for( m in 1:4 ) {
-      mu.beta[m] ~ dnorm(0, 1E-4)
-    }
-    
-    for( t in 1:6 ) {
-      tau[t] ~ dgamma(1E-3, 1E-3)
-    }
-    
-    sigma <- 1 / sqrt(tau)
-    
-    }"
-, fill=TRUE)
-sink()
-
-
-
-sink("growth_2level_NCI_TNCI.bug")
-
-cat(" model {
-    
-    for( i in 1:ntree ) {
-    
-    growth[i] ~ dnorm(mu[i], tau[1])
-
-    mu[i] <- beta.1[species[i]]
-            + beta.2[species[i]] * nci[i]
-            + beta.3[species[i]] * tnci[i]
-            + beta.4[species[i]] * dbh[i]
-            + indiv.effect[indiv[i]]
-    }
-    
-    for( j in 1:nspecies ) {
-      beta.1[j] ~ dnorm(mu.beta[1] + beta.t[1] * trait[j], tau[2])
-      beta.2[j] ~ dnorm(mu.beta[2] + beta.t[2] * trait[j], tau[3])
-      beta.3[j] ~ dnorm(mu.beta[3], tau[4])
-      beta.4[j] ~ dnorm(mu.beta[4], tau[5])
-    }
-    
-    for( i.a in 1:nindiv ) {
-      indiv.effect[i.a] ~ dnorm(0, tau[6])
-    }
-    
-    for(b in 1:2){
-      beta.t[b] ~ dnorm(0, 1E-4)
-    }
-    
-    for(m in 1:4){
+    for( m in 1:3 ) {
     mu.beta[m] ~ dnorm(0, 1E-4)
     }
-
-    for(t in 1:6){
-      tau[t] ~ dgamma(1E-3, 1E-3)
+    
+    for( t in 1:10 ) {
+    tau[t] ~ dgamma(1E-3, 1E-3)
     }
     
     sigma <- 1 / sqrt(tau)
     
     }"
-, fill=TRUE)
+    , fill=TRUE)
 sink()
-
 
 ################################################
 ### Set initial values, monitors, iterations and run model ###
@@ -280,8 +190,8 @@ sink()
 inits <- function (){
   list(
     beta.t = rnorm(2),
-    mu.beta = rnorm(4),
-    tau = rgamma(6, 1E3, 1E3))
+    mu.beta = rnorm(3),
+    tau = rgamma(10, 1E3, 1E3))
 }
 
 if(pc==T){ 
@@ -291,8 +201,7 @@ if(pc==T){
 }
 
 # Set monitors
-# params <- c(paste("beta",1:4,sep='.'),"beta.t","mu.beta","sigma")
-params <- c("beta.t","mu.beta","sigma")
+params <- c(paste("beta.t.",1:2,sep=''), paste("mu.beta.",1:3,sep=''),"beta.t","mu.beta","sigma")
 
 # Run model
 adapt <- 1000
@@ -302,7 +211,7 @@ thin <- 5
 chains <- 3
 
 mod <- jagsUI::jags(data, inits, params, 
-                    "growth_2level_NCI_TNCI.bug", 
+                    "growth_3level_NCI.bug", 
                     n.chains=chains, n.adapt=adapt, n.iter=iter, 
                     n.burnin=burn, n.thin=thin, parallel=F)
 
@@ -344,18 +253,9 @@ assign(paste(focal.plot,trait,sep='_'), readRDS(file))
 
 
 
-
-
-
-
-
-
-
-
-
 plot.coeffs <- function(mod){
   x <- cbind(unlist(mod$q50),unlist(mod$q2.5),unlist(mod$q97.5))
-  x <- x[-nrow(x),]
+  x <- x[-grep('deviance',rownames(x)),]
   x <- x[-grep('sigma',rownames(x)),]
   bg <- ifelse(sign(x[,2])==sign(x[,3]), 1, 0)
   plot(x[,1],ylim=c(min(x), max(x)), pch=21, bg=bg, axes=F, ylab='Std. Effect', xlab='', cex=1.5)
@@ -363,7 +263,8 @@ plot.coeffs <- function(mod){
   abline(h=0,lty=2)
   axis(1, labels=rownames(x), at=1:nrow(x), las=2)
   axis(2); box()
-  n <- lapply(mod$sims.list[1:2], function(x) apply(x, 2, quantile, c(0.05, 0.95)))
+  s <- which(!names(mod$sims.list) %in% c('sigma', 'deviance'))
+  n <- lapply(mod$sims.list[s], function(x) apply(x, 2, quantile, c(0.05, 0.95)))
   b <- unlist(lapply(n, function(x)x[1,]))
   t <- unlist(lapply(n, function(x)x[2,]))
   segments(1:nrow(x), b, 1:nrow(x), t, lwd=4)
@@ -464,5 +365,6 @@ load('testmod.RDA')
 plotmod
 
 mod2 <- update(plotmod, n.iter=100)
+
 
 
