e383fa44c3afdc61a613ea83a6c21a2eff9799e3
Bob Muscarella <bob.muscarella@gmail.com>
2016-06-17 17:40:01 +0200

    update plotting

--- 3a) Figures_6.17.16.R
+++ 3a) Figures_6.17.16.R
@@ -11,31 +11,19 @@ m <- c('growth','survival')[j]
 for(i in 1:6){
 file <- list.files(m)[i]
 r <- readRDS(paste(m,file,sep='/'))
-q50 <- round(unlist(r$q50), 3)
-q50 <- q50[!grepl('sigma',names(q50))]
-q50 <- q50[!grepl('deviance',names(q50))]
-
-q2.5 <- round(unlist(r$q2.5), 3)
-q2.5 <- q2.5[!grepl('sigma',names(q2.5))]
-q2.5 <- q2.5[!grepl('deviance',names(q2.5))]
-
-q97.5 <- round(unlist(r$q97.5), 3)
-q97.5 <- q97.5[!grepl('sigma',names(q97.5))]
-q97.5 <- q97.5[!grepl('deviance',names(q97.5))]
-
+samp <- do.call(rbind, r$samples)
+samp <- samp[,!grepl('sigma', colnames(samp))]
+samp <- samp[,!grepl('deviance', colnames(samp))]
+q <- round(apply(samp, 2, quantile, c(0.025, 0.05, 0.5, 0.95, 0.975)), 3)
 s <- substring(file, 5, 6)
 p <- substring(file, 1, 3)
-
-param <- names(q50)
-res <- rbind(res, data.frame(m, s, p, param, q50, q2.5, q97.5))
-}
+param <- colnames(q)
+rownames(q) <- paste('q',c(2.5, 5, 50, 95, 97.5),sep='')
+res <- rbind(res, data.frame(m, s, p, param, t(q)))
+}}
 
 rownames(res) <- NULL
-}
-
-
 res$p <- ifelse(res$p=='coc',1,ifelse(res$p=='bci',2,3))
-
 
 ########################################################
 ########## TRAIT EFFECT ON SENSITIVITY TO NCI ##########
@@ -43,18 +31,19 @@ res$p <- ifelse(res$p=='coc',1,ifelse(res$p=='bci',2,3))
 
 ##### GET GROWTH RESULTS
 g <- res[res$m=='growth',]
-b <- c('beta.wd2', 'beta.lma2')
+b <- c('beta.wd[2]', 'beta.lma[2]')
 d <- g[as.character(g$param) %in% b,]
 d <- d[order(d$param, d$p, d$s),]
 
 par(mfrow=c(2,1), mar=c(0,4,0.25,0), oma=c(5,3,4,1))
 colreps <- rep(rep(col, each=2), times=2)
-bg <- ifelse(sign(d$q2.5)==sign(d$q97.5), colreps, 'white')
+bg <- ifelse(sign(d$q2.5)==sign(d$q97.5), colreps, ifelse(sign(d$q5)==sign(d$q95), 'grey', 'white'))
 xs <- rep(1:(nrow(d)/2), each=2) + rep(c(0, 0.25), times=(nrow(d)/2))
 ylim <- c(min(d$q2.5, na.rm=T) - (2*sd(d$q50)), max(d$q97.5, na.rm=T) + (2*sd(d$q50)))
 plot(xs, d$q50, ylim=ylim, xlim=range(xs), col=NA, axes=F, xlab='', ylab='')
 abline(h=0, lty=2)
 arrows(xs, d$q2.5, xs, d$q97.5, len=0.1, angle=90, code=3, col=colreps, lwd=2)
+segments(xs, d$q5, xs, d$q95, col=colreps, lwd=5)
 points(xs, d$q50, pch=21, bg=bg, col=colreps, cex=1.5)
 abline(v=mean(xs))
 axis(2); box()
@@ -67,18 +56,19 @@ axis(1, labels=rep(c('',''), 6), at=xs, cex.axis=1.5, las=2, tck = 0.03)
 
 ##### GET SURVIVAL RESULTS
 g <- res[res$m=='survival',]
-b <- c('beta.wd2', 'beta.lma2')
+b <- c('beta.wd[2]', 'beta.lma[2]')
 d <- g[as.character(g$param) %in% b,]
 d <- d[order(d$param, d$p, d$s),]
 
 #par(mfrow=c(2,1), mar=c(0,4,0.25,0), oma=c(5,3,4,1))
 colreps <- rep(rep(col, each=2), times=2)
-bg <- ifelse(sign(d$q2.5)==sign(d$q97.5), colreps, 'white')
+bg <- ifelse(sign(d$q2.5)==sign(d$q97.5), colreps, ifelse(sign(d$q5)==sign(d$q95), 'grey', 'white'))
 xs <- rep(1:(nrow(d)/2), each=2) + rep(c(0, 0.25), times=(nrow(d)/2))
 ylim <- c(min(d$q2.5, na.rm=T) - (2*sd(d$q50)), max(d$q97.5, na.rm=T) + (2*sd(d$q50)))
 plot(xs, d$q50, ylim=ylim, xlim=range(xs), col=NA, axes=F, xlab='', ylab='')
 abline(h=0, lty=2)
 arrows(xs, d$q2.5, xs, d$q97.5, len=0.1, angle=90, code=3, col=colreps, lwd=2)
+segments(xs, d$q5, xs, d$q95, col=colreps, lwd=5)
 points(xs, d$q50, pch=21, bg=bg, col=colreps, cex=1.5)
 abline(v=mean(xs))
 axis(2); box()
@@ -98,18 +88,19 @@ axis(1, labels=rep(c('',''), 6), at=xs, cex.axis=1.5, las=2, tck = 0.03)
 
 ##### GET GROWTH RESULTS
 g <- res[res$m=='growth',]
-b <- c('beta.wd1', 'beta.lma1')
+b <- c('beta.wd[1]', 'beta.lma[1]')
 d <- g[as.character(g$param) %in% b,]
 d <- d[order(d$param, d$p, d$s),]
 
 par(mfrow=c(2,1), mar=c(0,4,0.25,0), oma=c(5,3,4,1))
 colreps <- rep(rep(col, each=2), times=2)
-bg <- ifelse(sign(d$q2.5)==sign(d$q97.5), colreps, 'white')
+bg <- ifelse(sign(d$q2.5)==sign(d$q97.5), colreps, ifelse(sign(d$q5)==sign(d$q95), 'grey', 'white'))
 xs <- rep(1:(nrow(d)/2), each=2) + rep(c(0, 0.25), times=(nrow(d)/2))
 ylim <- c(min(d$q2.5, na.rm=T) - (2*sd(d$q50)), max(d$q97.5, na.rm=T) + (2*sd(d$q50)))
 plot(xs, d$q50, ylim=ylim, xlim=range(xs), col=NA, axes=F, xlab='', ylab='')
 abline(h=0, lty=2)
 arrows(xs, d$q2.5, xs, d$q97.5, len=0.1, angle=90, code=3, col=colreps, lwd=2)
+segments(xs, d$q5, xs, d$q95, col=colreps, lwd=5)
 points(xs, d$q50, pch=21, bg=bg, col=colreps, cex=1.5)
 abline(v=mean(xs))
 axis(2); box()
@@ -122,17 +113,18 @@ axis(1, labels=rep(c('',''), 6), at=xs, cex.axis=1.5, las=2, tck = 0.03)
 
 ##### GET SURVIVAL RESULTS
 g <- res[res$m=='survival',]
-b <- c('beta.wd1', 'beta.lma1')
+b <- c('beta.wd[1]', 'beta.lma[1]')
 d <- g[as.character(g$param) %in% b,]
 d <- d[order(d$param, d$p, d$s),]
 
 colreps <- rep(rep(col, each=2), times=2)
-bg <- ifelse(sign(d$q2.5)==sign(d$q97.5), colreps, 'white')
+bg <- ifelse(sign(d$q2.5)==sign(d$q97.5), colreps, ifelse(sign(d$q5)==sign(d$q95), 'grey', 'white'))
 xs <- rep(1:(nrow(d)/2), each=2) + rep(c(0, 0.25), times=(nrow(d)/2))
 ylim <- c(min(d$q2.5, na.rm=T) - (2*sd(d$q50)), max(d$q97.5, na.rm=T) + (2*sd(d$q50)))
 plot(xs, d$q50, ylim=ylim, xlim=range(xs), col=NA, axes=F, xlab='', ylab='')
 abline(h=0, lty=2)
 arrows(xs, d$q2.5, xs, d$q97.5, len=0.1, angle=90, code=3, col=colreps, lwd=2)
+segments(xs, d$q5, xs, d$q95, col=colreps, lwd=5)
 points(xs, d$q50, pch=21, bg=bg, col=colreps, cex=1.5)
 abline(v=mean(xs))
 axis(2); box()
